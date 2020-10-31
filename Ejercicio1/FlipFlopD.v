936721b2dc097a6ad686301524ccabe65d7df297
vel18352 <68131403+vel18352@users.noreply.github.com>
2020-10-31 10:01:12 -0600

    Update FlipFlopD.v

--- Ejercicio1/FlipFlopD.v
+++ Ejercicio1/FlipFlopD.v
@@ -9,13 +9,13 @@ module FlipFlop1b (input clk, reset, eneable, D, output reg Q);
     end
 endmodule
 
-//Flip Flop 2 bits
+//FlipFlopD 2bits
 module FlipFlop2b (input clk, reset, eneable, input [1:0] D, output [1:0] Q);
     FlipFlop1b FF1(clk, reset, eneable, D [1], Q [1]);
     FlipFlop1b FF2(clk, reset, eneable, D [0], Q [0]);
 endmodule
 
-//Flip Flop D 4 bits
+//FlipFlopD 4bits
 module FlipFlop4b (input clk, reset, eneable, input [3:0] D, output [3:0] Q);
     FlipFlop2b FF3(clk, reset, eneable, D [3:2], Q [3:2]);
     FlipFlop2b FF4(clk, reset, eneable, D [1:0], Q [1:0]);
